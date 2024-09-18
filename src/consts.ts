4b202902b0343678432a65818a690f2458d53b7c
Amirhossein Banavi <ahbanavi@gmail.com>
2024-09-18 14:53:54 +0330

    change config to add social

--- src/consts.ts
+++ src/consts.ts
@@ -1,16 +1,16 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Astro Nano",
-  EMAIL: "markhorn.dev@gmail.com",
+  NAME: "Yedoost",
+  EMAIL: "ahbanavi@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 3,
-  NUM_WORKS_ON_HOMEPAGE: 2,
+  NUM_WORKS_ON_HOMEPAGE: 3,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
 export const HOME: Metadata = {
-  TITLE: "Home",
-  DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
+  TITLE: "Yedoost",
+  DESCRIPTION: "Amir Hossein Banavi (Yedoost) personal website and portfolio.",
 };
 
 export const BLOG: Metadata = {
@@ -29,16 +29,24 @@ export const PROJECTS: Metadata = {
 };
 
 export const SOCIALS: Socials = [
-  { 
-    NAME: "twitter-x",
-    HREF: "https://twitter.com/markhorn_dev",
-  },
-  { 
+  {
     NAME: "github",
-    HREF: "https://github.com/markhorn-dev"
+    HREF: "https://github.com/ahbanavi",
   },
-  { 
+  {
+    NAME: "twitter",
+    HREF: "https://x.com/iyedoost",
+  },
+  {
+    NAME: "telegram",
+    HREF: "https://t.me/yedoost",
+  },
+  {
+    NAME: "instagram",
+    HREF: "https://www.instagram.com/ahbanavi",
+  },
+  {
     NAME: "linkedin",
-    HREF: "https://www.linkedin.com/in/markhorn-dev",
-  }
+    HREF: "https://www.linkedin.com/in/ahbanavi",
+  },
 ];
