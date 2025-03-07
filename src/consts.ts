14f3c41623492b615c0d42f510571045aab7b1ba
Amirhossein Banavi <ahbanavi@gmail.com>
2025-03-07 15:42:01 +0330

    Add default image support and integrate SEO component in blog layout

--- src/consts.ts
+++ src/consts.ts
@@ -6,6 +6,7 @@ export const SITE: Site = {
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
+  DEFAULT_IMAGE: 'https://avatars.githubusercontent.com/u/39927312',
 };
 
 export const HOME: Metadata = {
